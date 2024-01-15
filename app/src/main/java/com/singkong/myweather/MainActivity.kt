ce9da0c861d9cfcf36c478a9a739cd3b5be5b2f4
Tommy Sing <>
2024-01-14 22:48:10 -0800

    Using NavHost on the home screen

--- app/src/main/java/com/singkong/myweather/MainActivity.kt
+++ app/src/main/java/com/singkong/myweather/MainActivity.kt
@@ -4,25 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.singkong.myweather.compose.WeatherListScreen
-import com.singkong.myweather.data.Location
-import com.singkong.myweather.data.UserPreferences
+import com.singkong.myweather.compose.MyWeatherApp
 import com.singkong.myweather.ui.theme.MyWeatherTheme
 import com.singkong.myweather.viewmodels.WeatherListViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -49,43 +36,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MainScreen(viewModel)
+                    MyWeatherApp(viewModel)
                 }
             }
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun MainScreen(
-    viewModel: WeatherListViewModel
-) {
-    val locationAndWeatherLogsList by viewModel.locationWeatherLogsList.observeAsState(initial = emptyList())
-    val userPreferences by viewModel.userPreferences.observeAsState(initial = UserPreferences(0))
 
-    val locationList = mutableListOf<Location>()
-    for (item in locationAndWeatherLogsList) {
-        locationList.add(item.location)
-    }
-    viewModel.refreshForecast(locationList)
-
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                colors = topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary),
-                title = { Text(text = stringResource(id = R.string.app_name)) }
-            )
-        }
-    ) { paddingValues ->
-        Column (
-            modifier = Modifier.padding(paddingValues),
-        ) {
-            WeatherListScreen(locationWeatherLogsList = locationAndWeatherLogsList, userPreferences, onDeleteLocation = {
-                viewModel.deleteLocation(it)
-            })
-        }
-    }
-}
---
+++ app/src/main/java/com/singkong/myweather/compose/MyWeatherApp.kt
@@ -0,0 +1,23 @@
+package com.singkong.myweather.compose
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.singkong.myweather.viewmodels.WeatherListViewModel
+
+@Composable
+fun MyWeatherApp(viewModel: WeatherListViewModel) {
+    val navController = rememberNavController()
+    MyWeatherNavHost(navController, viewModel)
+}
+
+@Composable
+fun MyWeatherNavHost(navController: NavHostController, viewModel: WeatherListViewModel) {
+    NavHost(navController = navController, startDestination = Screen.Home.route) {
+        composable(route = Screen.Home.route) {
+            HomeScreen(viewModel)
+        }
+    }
+}
